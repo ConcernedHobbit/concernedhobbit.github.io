0a850ae6e6e886a6ee676a6a38a1f62ee51e377a
Eetu Raunio <me@concernedhobbit.eu>
2019-07-19 23:00:10 +0300

    Added pogo folder
    /pogo/ serves as a Pokémon Go battle CP generator, mostly for friend group use

---
+++ pogo/generator.js
@@ -0,0 +1,66 @@
+var rand = function(min, max) {
+    let value = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(value);
+    return value;
+}
+
+var generate = function(generator, mass=false) {
+    let controls = generator.getElementsByClassName('controls').item(0);
+    let output = generator.getElementsByTagName('output').item(0);
+
+    let inputs = controls.getElementsByTagName('input');
+    let min = parseInt(inputs.item(0).value);
+    let max = parseInt(inputs.item(1).value);
+
+    let value = rand(min, max)
+    output.value = value;
+
+    if (!mass) {
+        let uri = generateSharableURI();
+        document.getElementById("shareWA").href = uri;
+    }
+
+    return value;
+}
+
+var generateAll = function() {
+    var generators = document.getElementsByClassName('generator');
+    for (let generator of generators) {
+        generate(generator, true);
+    }
+
+    let uri = generateSharableURI();
+    document.getElementById("shareWA").href = uri;
+}
+
+var generateSharableURI = function() {
+    var generators = document.getElementsByClassName('generator');
+
+    let leagues = [];
+
+    for (let generator of generators) {
+        let league = generator.getElementsByTagName('h3').item(0).innerText;
+        let cp = generator.getElementsByTagName('output').item(0).value;
+
+        leagues.push([league, cp]);
+    }
+
+    let str = "https://wa.me/?text=";
+
+    for (let league of leagues) {
+        let name = league[0];
+        let cp = league[1];
+
+        str += name + ": CP " + cp + "\r\n";
+    }
+
+    return encodeURI(str);
+}
+
+var generateButtons = document.getElementsByClassName('generate');
+
+for (let button of generateButtons) {
+    button.addEventListener('click', function(evt) {
+        generate(evt.path[2]);
+    });
+}
